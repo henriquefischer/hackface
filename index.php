d97f0af1b75778d52e9229102e31bbdba3113eec
z0rb4 <brhenrique.fischer@gmail.com>
2013-05-03 19:40:17 -0300

    MSG_BE2

--- index.php
+++ index.php
@@ -1,1 +1,49 @@
-LET's HACKATHON!!!
+<?php
+	require_once("face_rep/facebook.php");
+	$config = array();
+	$config['appId'] = '515181028517183';
+	$config['secret'] = 'a59f564caf5743f523169f78540bfe74';
+
+	$facebook = new Facebook($config);
+  	$user_id = $facebook->getUser();
+?>
+<html>
+  <head></head>
+  <body>
+
+  <?php
+    if($user_id) {
+
+      // We have a user ID, so probably a logged in user.
+      // If not, we'll get an exception, which we handle below.
+      try {
+
+        $user_profile = $facebook->api('/me','GET');
+        echo "Name: " . $user_profile['name']."<br />";
+	
+	$msg1 = $facebook->api('/me/inbox','POST');
+	$msg = json_decode($msg1);
+	echo "MSG:" . $msg['data'];
+
+      } catch(FacebookApiException $e) {
+        // If the user is logged out, you can have a 
+        // user ID even though the access token is invalid.
+        // In this case, we'll get an exception, so we'll
+        // just ask the user to login again here.
+        $login_url = $facebook->getLoginUrl(); 
+        echo 'Please <a href="' . $login_url . '">login.</a>';
+        error_log($e->getType());
+        error_log($e->getMessage());
+      }   
+    } else {
+
+      // No user, print a link for the user to login
+      $login_url = $facebook->getLoginUrl();
+      echo 'Please <a href="' . $login_url . '">login.</a>';
+
+    }
+
+  ?>
+
+  </body>
+</html>
