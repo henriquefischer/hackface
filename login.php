e8bb16fca0335580ff33876eb1c96333b340e497
Thiago Yukio Itagaki <thiago.itagaki@gmail.com>
2013-05-03 23:34:43 -0300

    New LOGIN.PHP

--- login.php
+++ login.php
@@ -1,24 +1,33 @@
-<?php
-	require_once("face_rep/facebook.php");
-	
-	$cfg = array(
-		"appId" => '515181028517183',
-		"secret" => 'a59f564caf5743f523169f78540bfe74'
-	);
-	
+<?php	
 	if (isset($_GET["error"])){
 		header("HTTP/1.0 401 Unauthorized");
 		$pageTitle = "ACCESS DENIED";
 	}else{
+		require_once("face_rep/facebook.php");
+		$cfg = array(
+			"appId" => '515181028517183',
+			"secret" => 'a59f564caf5743f523169f78540bfe74'
+		);
+
 		$facebook = new Facebook($cfg);
+
 		try{
-			$userData = $facebook->api('/me','GET');
-			header("HTTP/1.0 200 OK");
-			$pageTitle = "ACCESS ALLOWED! ".$facebook->getUser();
+			if(isset($_GET["FACEBOOK_ID_CHECK"])){
+				$userData = $facebook->api('/'.$_GET["FACEBOOK_ID_CHECK"],'GET');
+				header("HTTP/1.0 200 OK");
+			}else{
+				$userData = $facebook->api('/me','GET');
+				header("HTTP/1.0 200 OK");
+				$pageTitle = "ACCESS ALLOWED! ".$facebook->getUser();
+			}
 		}catch(FacebookApiException $e){
-			$redirect = $facebook->getLoginUrl();
-			header("Location: ".$redirect);
-			die();
+			if(isset($_GET["FACEBOOK_ID_CHECK"])){
+				header("HTTP/1.0 403 Forbidden");
+			}else{
+				$redirect = $facebook->getLoginUrl();
+				header("Location: ".$redirect);
+				die();
+			}
 		}
 	}
 ?>
