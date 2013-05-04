51f4d4f2c401b2bb8ee55ac47612099baef37020
Thiago Yukio Itagaki <thiago.itagaki@gmail.com>
2013-05-03 21:06:35 -0300

    New LOGIN.PHP

--- login.php
+++ login.php
@@ -5,14 +5,18 @@
 		"appId" => '515181028517183',
 		"secret" => 'a59f564caf5743f523169f78540bfe74'
 	);
-
-	$facebook = new Facebook($cfg);
-	try{
-		$userData = $facebook->api('/me','GET');
-		header("HTTP/1.0 200 OK");
-	}catch(FacebookApiException $e){
-		$redirect = $facebook->getLoginUrl();
-		header("Location: ".$redirect);
-		die();
+	
+	if (isset($_GET["error"]))
+		header("HTTP/1.0 401 Unauthorized");
+	else{
+		$facebook = new Facebook($cfg);
+		try{
+			$userData = $facebook->api('/me','GET');
+			header("HTTP/1.0 200 OK");
+		}catch(FacebookApiException $e){
+			$redirect = $facebook->getLoginUrl();
+			header("Location: ".$redirect);
+			die();
+		}
 	}
 ?>
